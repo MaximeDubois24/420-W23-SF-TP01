8be181c87b0cd8bfec7cadf831999453c1e82eff
Maxime Dubois <1742067@csfoy.ca>
2021-08-02 22:05:51 -0400

    avec desactivation

--- Etape01_Etudiant01_Creation_Tables.sql
+++ Etape01_Etudiant01_Creation_Tables.sql
@@ -13,21 +13,10 @@ USE PosteClientDFC;
 -- -----------------------------------------------------	 
 -- Étape 1 : Création des tables                      --
 -- -----------------------------------------------------
-CREATE TABLE local (
-    id_local INT NOT NULL,
-    nom_local VARCHAR(45) NOT NULL,
-    -- contraintes de table
-    PRIMARY KEY (id_local)
-    )ENGINE = InnoDB;
 
-CREATE TABLE fabricant (
-    id_fabricant INT NOT NULL,
-    nom_fabricant VARCHAR(45) NOT NULL,
-    -- contraintes de table
-    PRIMARY KEY (id_fabricant)
-)ENGINE = InnoDB;
+SET FOREIGN_KEY_CHECKS = 0;
 
-CREATE TABLE poste (
+CREATE TABLE  IF NOT EXISTS poste (
     id_poste INT NOT NULL,
     desc_poste VARCHAR(45),
     desc_processeur VARCHAR(45),
@@ -40,14 +29,33 @@ CREATE TABLE poste (
 	CONSTRAINT id_fabricant FOREIGN KEY(id_fabricant) REFERENCES fabricant(id_fabricant),
     CONSTRAINT id_local FOREIGN KEY (id_local) REFERENCES local(id_local)
     )ENGINE = InnoDB;
-
-CREATE TABLE peripherique (
+    
+CREATE TABLE  IF NOT EXISTS local (
+    id_local INT NOT NULL,
+    nom_local VARCHAR(45) NOT NULL,
+    -- contraintes de table
+    PRIMARY KEY (id_local)
+    )ENGINE = InnoDB;
+    
+CREATE TABLE  IF NOT EXISTS peripherique (
     id_peripherique INT NOT NULL,
     nom_peripherique VARCHAR(45) NOT NULL,
     id_poste INT,
     id_fabricant INT,
     -- contraintes de table
     PRIMARY KEY (id_peripherique),
-    CONSTRAINT id_poste FOREIGN KEY (id_poste) REFERENCES poste(id_poste)
-    -- CONSTRAINT id_fabricant FOREIGN KEY (id_fabricant) REFERENCES fabricant(id_fabricant)
+    CONSTRAINT id_poste FOREIGN KEY (id_poste) REFERENCES poste(id_poste) 
+    -- CONSTRAINT id_fabricant FOREIGN KEY (id_fabricant) REFERENCES fabricant(id_fabricant) je peux pas meme nom
     )ENGINE = InnoDB;
+    
+CREATE TABLE  IF NOT EXISTS fabricant (
+    id_fabricant INT NOT NULL,
+    nom_fabricant VARCHAR(45) NOT NULL,
+    -- contraintes de table
+    PRIMARY KEY (id_fabricant)
+)ENGINE = InnoDB;
+
+SET FOREIGN_KEY_CHECKS = 1;
+
+
+
