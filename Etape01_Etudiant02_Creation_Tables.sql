2198212398e664ca2f68fefecd7cfcecc0c7a5a4
unknown <reda.bouabdallah420@gmail.com>
2021-08-03 11:25:43 +0800

    version final

--- Etape01_Etudiant02_Creation_Tables.sql
+++ Etape01_Etudiant02_Creation_Tables.sql
@@ -1,14 +1,51 @@
 /* 
  Travail pratique 1
  Nom de la première personne
- --> Nom de la deuxième personne
+ --> Mohamed Reda Bouabdallah
  
- Date  JJ-MM-AAAA                                             
+ Date  02-08-2021                                             
 */
--- DROP DATABASE IF EXISTS PosteClientDFC;
--- CREATE DATABASE IF NOT EXISTS PosteClientDFC;
+DROP DATABASE IF EXISTS PosteClientDFC;
+CREATE DATABASE IF NOT EXISTS PosteClientDFC;
 
 USE PosteClientDFC;
 -- -----------------------------------------------------
 -- Étape 1 : Création des tables                      --
 -- -----------------------------------------------------
+
+CREATE TABLE IF NOT EXISTS logiciel (
+id_logiciel INT UNSIGNED NOT NULL AUTO_INCREMENT,
+nom_logiciel VARCHAR(45) NOT NULL,
+nb_licence INT,
+id_editeur INT,
+PRIMARY KEY (id_logiciel),
+CONSTRAINT  FOREIGN KEY (id_editeur) 
+REFERENCES editeur(id_editeur)
+) ENGINE = InnoDB;
+
+CREATE TABLE IF NOT EXISTS editeur(
+id_editeur INT UNSIGNED NOT NULL AUTO_INCREMENT,
+nom_editeur VARCHAR(45) NOT NULL,
+PRIMARY KEY (id_editeur)
+) ENGINE = InnoDB;
+
+CREATE TABLE IF NOT EXISTS usager (
+id_usager INT UNSIGNED NOT NULL AUTO_INCREMENT,
+nom_usager VARCHAR(45) NOT NULL,
+prenom_usager VARCHAR(45) NOT NULL,
+telephone_usager VARCHAR(15),
+id_local INT,
+PRIMARY KEY (id_usager),
+CONSTRAINT  FOREIGN KEY (id_local) 
+REFERENCES local(id_local)
+) ENGINE = InnoDB;
+
+CREATE TABLE IF NOT EXISTS poste_has_logiciel(
+id_poste INT UNSIGNED NOT NULL AUTO_INCREMENT,
+id_logiciel INT NOT NULL AUTO_INCREMENT
+) ENGINE = InnoDB;
+
+CREATE TABLE IF NOT EXISTS usager_has_poste(
+id_usager INT UNSIGNED NOT NULL AUTO_INCREMENT,
+id_poste INT NOT NULL AUTO_INCREMENT
+) ENGINE = InnoDB;
--- Etape03_Etudiant02_Ajout_Tables_Colonnes.sql
+++ Etape03_Etudiant02_Ajout_Tables_Colonnes.sql
@@ -10,3 +10,19 @@ USE PosteClientDFC;
 -- -----------------------------------------------------
 -- Étape 3 : Ajout de tables et de colonnes           --
 -- -----------------------------------------------------
+
+CREATE TABLE IF NOT EXISTS typeusager(
+id_typeusager INT UNSIGNED NOT NULL AUTO_INCREMENT,
+nom_statut VARCHAR(45),
+PRIMARY KEY (id_typeusager)
+) ENGINE = InnoDB;
+
+ALTER TABLE usager
+ADD id_typeusager INT,
+ADD CONSTRAINT id_typeusager FOREIGN KEY (id_typeusager) REFERENCES typeusager(id_typeusager);
+
+ALTER TABLE logiciel
+MODIFY nom_logiciel VARCHAR(45) UNIQUE NOT NULL;
+
+ALTER TABLE editeur
+MODIFY nom_editeur VARCHAR(45) UNIQUE NOT NULL; 
